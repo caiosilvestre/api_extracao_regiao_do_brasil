e77d2b12b0ff6154548459078841f5c38a0a5d37
caiosilvestre <scaioimperial@gmail.com>
2022-10-28 12:59:56 -0300

    update api send

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask, send_file
+import os
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def download_csv():
-    csv = 'Arquivo enviado  .csv'
-    response = make_response(csv)
-    cd = 'attachment; filename=DataFrame_Regiao.csv'
-    response.headers['Content-Disposition'] = cd
-    response.mimetype='text/csv'
+    csv_path = os.path.join('./DataFrame_Regiao.csv')
 
-    return response
+    return send_file(csv_path, as_attachment=True)
